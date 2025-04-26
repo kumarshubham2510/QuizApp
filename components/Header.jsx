a4c69d0a8c7cf8ca4f4f86da6725954965a4f3b9
Kumar Shubham <kumarshubhams2510@gmail.com>
2025-04-26 20:23:48 +0530

    headers and logo added

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+import logoImg from "../src/assets/quiz-logo.png";
+export default function Header() {
+  return (
+    <header>
+      <img src={logoImg} alt="Quiz Logo " />
+      <h1>QUIZ APP</h1>
+    </header>
+  );
+}
